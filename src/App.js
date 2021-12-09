bcff2c5207ced0998dc77ee704be7f20092828ea
Dhiia Karoui <Med.Dhiia@Gmail.com>
2021-12-10 00:11:38 +0100

    Add Loading Spinner when fetching personas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { useState, useEffect } from "react";
 import Cards from "./Components/Cards/Cards";
 import "./App.css";
+import { fetchPersonas } from "./Services/personas.service";
+import { Spinner } from "react-bootstrap";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [personas, setPersonas] = useState([]);
+
+  useEffect(() => {
+    // (async () => setTasks(await fetchTasks()))();
+    (async () => {
+      setIsLoading(true);
+      try {
+        setPersonas(await fetchPersonas());
+        setIsLoading(false);
+      } catch (e) {
+        console.error(e);
+        setIsError(true);
+        setIsLoading(false);
+        setIsVisible(false);
+      }
+    })();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
-        <Cards />
-        <Cards />
+        {isLoading && !isError && (
+          <Spinner className="loading" animation="border" />
+        )}
+        {!isLoading && !isError && isVisible && (
+          <>
+            <Cards />
+            <Cards />
+          </>
+        )}
       </div>
     </div>
   );
